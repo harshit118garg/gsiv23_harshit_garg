549a37d9e3267a95f9db9e28104aa50295e9589f
Harshit Garg <harshit118garg@gmail.com>
2023-08-19 21:26:08 +0530

    single movie css fixed

--- src/components/SingleMovie/index.tsx
+++ src/components/SingleMovie/index.tsx
@@ -15,42 +15,50 @@ export const SingleMovie = ({
 }: SingleMoviePropTypes) => {
   return (
     <>
-      {movie && (
-        <div className="single-movie">
-          <div className="movie-card-body">
-            <div className="img-part">
-              <img
-                src={
-                  movie?.poster_path
-                    ? `${IMG_PATH_300}${movie?.poster_path}`
-                    : IMG_NOT_AVAILABLE
-                }
-                alt={movie?.title}
-              />
+      <div className="single-movie">
+        <div className="movie-card-body">
+          <div className="img-part">
+            <img
+              src={
+                movie?.poster_path
+                  ? `${IMG_PATH_300}${movie?.poster_path}`
+                  : IMG_NOT_AVAILABLE
+              }
+              alt={movie?.title}
+            />
+          </div>
+          <div className="details-part">
+            <div className="row1">
+              <div className="head">
+                <h3>
+                  {movie?.title} <span>({movie?.vote_average})</span>
+                </h3>
+              </div>
             </div>
-            <div className="details-part">
-              <div className="row1">
-                <div className="head">
-                  {movie?.title} <span>{movie?.vote_average}</span>
-                </div>
-              </div>
-              <div className="row2">
-                <span>{movie?.release_date}</span> <br />
-                <span>{movie?.runtime}</span> <br />
-                <span>{movieDirector?.name}</span>
-              </div>
-              <div className="row3">
+            <div className="row2">
+              <p>
+                <span>Date of Release:</span> {movie?.release_date} |{" "}
+                <span>Length:</span> {movie?.runtime} minutes | <span>Director:</span>{" "}
+                {movieDirector?.name}
+              </p>
+            </div>
+            <div className="row3">
+              <p>
+                <span>Cast: </span>
                 {castMembers.map((cast: string, i: number) => {
                   return <span key={i}> {cast} ,</span>;
                 })}
-              </div>
-              <div className="row4">
-                <p>{movie?.overview}</p>
-              </div>
+              </p>
+            </div>
+            <div className="row4">
+              <p>
+                <span>Description: </span>
+                {movie?.overview}
+              </p>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
